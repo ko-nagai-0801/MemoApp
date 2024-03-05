2456b9a34ec1c865830cd457d9d3d541f77ca888
koh <ko.nagai.0801@gmail.com>
2024-03-06 06:24:18 +0900

    Markup Memo LIst screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,73 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+      {/* メモの外枠 */}
+      <View>
+
+        {/* メモのアイテム */}
+        <View>
+          {/* テキスト */}
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年4月1日 10:00</Text>
+          </View>
+          {/* バツボタン */}
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        {/* メモのアイテム */}
+        <View>
+          {/* テキスト */}
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年4月1日 10:00</Text>
+          </View>
+          {/* バツボタン */}
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        {/* メモのアイテム */}
+        <View>
+          {/* テキスト */}
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年4月1日 10:00</Text>
+          </View>
+          {/* バツボタン */}
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+      </View>
+
+      {/* +ボタン */}
+      <View>
+        <Text>+</Text>
+      </View>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default Index
