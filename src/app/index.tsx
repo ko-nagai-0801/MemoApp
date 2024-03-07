215f5ed38ad7de73442a754601a691cf3e061bb4
koh <ko.nagai.0801@gmail.com>
2024-03-08 06:35:11 +0900

    Add Button Style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -16,11 +16,11 @@ const Index = (): JSX.Element => {
       <View>
 
         {/* メモのアイテム */}
-        <View>
+        <View style={styles.memoListItem}>
           {/* テキスト */}
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2024年4月1日 10:00</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2024年4月1日 10:00</Text>
           </View>
           {/* バツボタン */}
           <View>
@@ -29,11 +29,11 @@ const Index = (): JSX.Element => {
         </View>
 
         {/* メモのアイテム */}
-        <View>
+        <View style={styles.memoListItem}>
           {/* テキスト */}
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2024年4月1日 10:00</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2024年4月1日 10:00</Text>
           </View>
           {/* バツボタン */}
           <View>
@@ -41,12 +41,12 @@ const Index = (): JSX.Element => {
           </View>
         </View>
 
-        {/* メモのアイテム */}
-        <View>
+                {/* メモのアイテム */}
+                <View style={styles.memoListItem}>
           {/* テキスト */}
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2024年4月1日 10:00</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2024年4月1日 10:00</Text>
           </View>
           {/* バツボタン */}
           <View>
@@ -57,8 +57,8 @@ const Index = (): JSX.Element => {
       </View>
 
       {/* +ボタン */}
-      <View>
-        <Text>+</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>＋</Text>
       </View>
 
     </View>
@@ -90,6 +90,46 @@ const styles = StyleSheet.create({
     lineHeight: 32,
     fontWeight: 'bold',
     color: '#ffffff'
+  },
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.15)'
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484'
+  },
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: '#467fd3',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    elevation: 8
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 48
   }
 })
 
