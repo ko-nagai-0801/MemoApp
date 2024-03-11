9a99c455e1b7919dabae965900b07cfbefe8b727
koh <ko.nagai.0801@gmail.com>
2024-03-12 05:50:12 +0900

    Edit memoDetail

---
+++ src/app/memo/list.tsx
@@ -0,0 +1,32 @@
+import { View, StyleSheet } from 'react-native'
+
+import Header from '../../components/Header'
+import MemoListItem from '../../components/MemoListItem'
+import CircleButton from '../../components/CircleButton'
+
+const List = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      {/* header */}
+      <Header />
+      {/* メモの外枠 */}
+      <View>
+        {/* メモのアイテム */}
+        <MemoListItem />
+        <MemoListItem />
+        <MemoListItem />
+      </View>
+      {/* +ボタン */}
+      <CircleButton>＋</CircleButton>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff'
+  }
+})
+
+export default List
